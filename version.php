13bbcb205c7c677ef538b86527b6769b70e53e5e
Daniel Nolte <daniel.nolte@coactum.de>
2017-12-01 20:44:16 +0100

    feat(version): increased plugin version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_exammanagement';
-$plugin->release = '0.1.0';
-$plugin->version = 2017091500;
+$plugin->release = '0.1.5';
+$plugin->version = 2017120100;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
