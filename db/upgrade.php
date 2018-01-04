69a63520954c7c81692d659a2ab175abaee7f8d3
Daniel Nolte <daniel.nolte@coactum.de>
2018-01-04 19:58:01 +0100

    fix(installation): fixed an error preventing the correct installation and upgrading process

--- db/upgrade.php
+++ db/upgrade.php
@@ -33,7 +33,7 @@ require_once(__DIR__.'/upgradelib.php');
  * @param int $oldversion
  * @return bool
  */
-function xmldb_mod_exammanagement_upgrade($oldversion) {
+function xmldb_exammanagement_upgrade($oldversion) {
     global $DB;
 
     $dbman = $DB->get_manager();
