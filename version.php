563eca5aa14a54bb299ba6cfec4ce3448294ff76
Daniel Nolte <daniel.nolte@coactum.de>
2018-01-05 13:54:44 +0100

    fix(version): forgot that we now have 2018

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_exammanagement';
 $plugin->release = '0.1.9';
-$plugin->version = 2017010500;
+$plugin->version = 2018010500;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
