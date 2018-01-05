9f8144e675d5acef6c3297468397665c3a8f9747
Daniel Nolte <daniel.nolte@coactum.de>
2018-01-05 20:57:19 +0100

    fix(install): testing installation process

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_exammanagement';
-$plugin->release = '0.1.9';
-$plugin->version = 2018010500;
+$plugin->release = '0.2.0';
+$plugin->version = 2018010600;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
