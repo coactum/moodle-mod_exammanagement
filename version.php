fefcb4a4eb40e35749d35b2fe2749019edd43f5a
Daniel Nolte <daniel.nolte@coactum.de>
2018-08-06 12:09:00 +0200

    fix(version): increased version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_exammanagement'; // Full name of the plugin (used for diagnostics)
-$plugin->release = '1.1.8';
-$plugin->version = 2018071500;	// The current module version (Date: YYYYMMDDXX)
+$plugin->release = '1.1.9';
+$plugin->version = 2018080600;	// The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2017051500; // Requires this Moodle version
 $plugin->maturity = MATURITY_ALPHA;
