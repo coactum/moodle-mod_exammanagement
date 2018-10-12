a2334507c4f28cbaaa8f0b5c29cce771f3d1f7f6
Daniel Nolte <daniel.nolte@coactum.de>
2018-10-12 19:10:30 +0200

    feat(ReworkDB): started reworking db structure for using multiple termbased tables

--- classes/forms/dateTimeForm.php
+++ classes/forms/dateTimeForm.php
@@ -24,6 +24,7 @@
 
 namespace mod_exammanagement\forms;
 use mod_exammanagement\general\exammanagementInstance;
+use mod_exammanagement\general\User; // for testing
 
 use moodleform;
 
@@ -34,6 +35,7 @@ global $CFG;
 require_once("$CFG->libdir/formslib.php");
 
 require_once(__DIR__.'/../general/exammanagementInstance.php');
+require_once(__DIR__.'/../general/User.php');
 
 class dateTimeForm extends moodleform {
 
@@ -54,6 +56,83 @@ class dateTimeForm extends moodleform {
         $mform->addElement('hidden', 'id', 'dummy');
         $mform->setType('id', PARAM_INT);
         $this->add_action_buttons();
+
+
+
+        function int_to_words($x) {
+          $nwords = array( "zero", "one", "two", "three", "four", "five", "six", "seven",
+                             "eight", "nine", "ten", "eleven", "twelve", "thirteen",
+                             "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+                             "nineteen", "twenty", 30 => "thirty", 40 => "forty",
+                             50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty",
+                             90 => "ninety" );
+
+           if(!is_numeric($x))
+              $w = '#';
+           else if(fmod($x, 1) != 0)
+              $w = '#';
+           else {
+              if($x < 0) {
+                 $w = 'minus ';
+                 $x = -$x;
+              } else
+                 $w = '';
+              // ... now $x is a non-negative integer.
+
+              if($x < 21)   // 0 to 20
+                 $w .= $nwords[$x];
+              else if($x < 100) {   // 21 to 99
+                 $w .= $nwords[10 * floor($x/10)];
+                 $r = fmod($x, 10);
+                 if($r > 0)
+                    $w .= '-'. $nwords[$r];
+              } else if($x < 1000) {   // 100 to 999
+                 $w .= $nwords[floor($x/100)] .' hundred';
+                 $r = fmod($x, 100);
+                 if($r > 0)
+                    $w .= ' and '. int_to_words($r);
+              } else if($x < 1000000) {   // 1000 to 999999
+                 $w .= int_to_words(floor($x/1000)) .' thousand';
+                 $r = fmod($x, 1000);
+                 if($r > 0) {
+                    $w .= ' ';
+                    if($r < 100)
+                       $w .= 'and ';
+                    $w .= int_to_words($r);
+                 }
+              } else {    //  millions
+                 $w .= int_to_words(floor($x/1000000)) .' million';
+                 $r = fmod($x, 1000000);
+                 if($r > 0) {
+                    $w .= ' ';
+                    if($r < 100)
+                       $word .= 'and ';
+                    $w .= int_to_words($r);
+                 }
+              }
+           }
+           return $w;
+        }
+
+        //test
+        var_dump('coursecategorienames');
+        global $DB;
+        $terms = $DB->get_fieldset_select('course_categories', 'name', '');
+        var_dump($terms);
+
+        var_dump(int_to_words('asd'));
+
+        foreach($terms as $termname){
+          var_dump('exammanagement_'.$termname);
+
+          $cleanTermname = preg_replace("/[^0-9a-zA-Z]/", "",$termname);
+          var_dump('exammanagement_'.$cleanTermname);
+        }
+
+        $UserObj = User::getInstance($this->_customdata['id'], $this->_customdata['e']);
+
+        var_dump($UserObj->getAllExamParticipantsIds());
+
     }
 
     //Custom validation should be added here
