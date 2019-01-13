e2a73b27265f0d70a0bfffc32baeaf1070fa4f3b
Daniel Nolte <daniel.nolte@coactum.de>
2019-01-13 19:39:49 +0100

    feat(deleteexamdata): worked on feature

--- classes/task/delete_old_exam_data.php
+++ classes/task/delete_old_exam_data.php
@@ -46,34 +46,64 @@ class delete_old_exam_data extends \core\task\scheduled_task {
 
         // send warning mails for soon to be deleted module instances
 
-        $warningperiodOne = strtotime("-1 month");
-        $warningperiodTwo = strtotime("-7 days");
-        $warningperiodThree = strtotime("-1 day");
-        $deletionDate = strtotime("-3 month");
+        $deletionDate = strtotime("+1 month");
+
+        $now = time();
+        $case = NULL;
+        var_dump(time());
 
 
+        ### variante 1: filter records in db via sql select and get only matching records (not working) #####
+        $select = "datadeletion IS NOT NULL AND TIMESTAMP(DATE_SUB(datadeletion, INTERVAL 1 MONTH)) <= ".$now; // not working
+        // SELECT * FROM `mdl_exammanagement` WHERE `datadeletion` IS NOT NULL AND TIMESTAMP(DATE_SUB(`datadeletion`, INTERVAL 1 MONTH)) > 1547146064 // in PHP-Myadmin getestet
+        $records = $MoodleDBObj->getFieldsetFromRecordsInDB('exammanagement', 'datadeletion', $select);
+        var_dump($records);
+        var_dump(date('d.m.y h:m', $records[0]));
 
-        var_dump($warningperiodOne);
-        var_dump($warningperiodTwo);
-        var_dump($warningperiodThree);
-        var_dump($deletionDate);
+        #### variante 2: get all records and filter in php
+        $select = "datadeletion IS NOT NULL AND datadeletion > ".$now; // not working
 
-        var_dump(date('d.m.y h:m', $deletionDate));
+        $records = $MoodleDBObj->getRecordsSelectFromDB('exammanagement', $select);
+        var_dump($records);
 
-        //$select = "correctioncompletiondate > UNIX_TIMESTAMP(DATE_SUB(NOW(),INTERVAL 180 DAY)";
-        $select = "correctioncompletiondate > ".$deletionDate;
+        foreach($records as $id => $record){
 
-        
-		$Test = $MoodleDBObj->getFieldsetFromRecordsInDB('exammanagement', 'correctioncompletiondate', $select);
+            // check if warning period is due
+            $warningperiodOne = strtotime("-1 month", $record->datadeletion);
+            $warningperiodTwo = strtotime("-7 days", $record->datadeletion);
+            $warningperiodThree = strtotime("-1 day", $record->datadeletion);
 
-        var_dump($Test);
+            var_dump($warningperiodOne);
+            var_dump($warningperiodTwo);
+            var_dump($warningperiodThree);
 
-        var_dump(date('d.m.y h:m', $Test[0]));
+            if($warningperiodOne <= $now){
+                $case = 1;
+                var_dump('should send first warning mail 1 month');
+            } else if($warningperiodTwo <= $now){
+                $case = 2;
+                var_dump('should send second warning mail 7 days');
+            } else if($warningperiodThree <= $now){
+                $case = 3;
+                var_dump('should send first warning mail 1 day');
+            }
 
+            var_dump($case);
 
-        //$MoodleDBObj->DeleteRecordsFromDBSelect($table, $select, $params)
+            // get user to whom warning mail should be send
+            
+            //$role = $MoodleDBObj->getRecordFromDB('role', array('shortname' => 'editingteacher'));
+            //$cmid = ... ;
+            //$modulecontext = get_context_instance(CONTEXT_COURSE, $cmid);
+            //$teachers = get_role_users($role->id, $modulecontext);
+
+            // send mail
+            // siehe send groupmessages
+        }
 
         // delete expired  exam data and instances
+        $select = "datadeletion IS NOT NULL AND datadeletion <= ".$now;
+        //$MoodleDBObj->DeleteRecordsFromDBSelect($table, $select);
         
         \core\task\manager::clear_static_caches(); // restart cron after running the task because it made many DB updates and clear cron cache (https://docs.moodle.org/dev/Task_API#Caches)
     }
