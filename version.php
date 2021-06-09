f5615d43f20b09963da4c859fc61f87fc4b20d25
Daniel Nolte <>
2021-06-09 14:12:03 +0200

    feat(version): increased version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_exammanagement'; // Full name of the plugin (used for diagnostics)
 $plugin->release = '1.4.4';
-$plugin->version = 2021052500;	// The current module version (Date: YYYYMMDDXX)
+$plugin->version = 2021060900;	// The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2020061500; // Requires this Moodle version
 $plugin->maturity = MATURITY_STABLE;
